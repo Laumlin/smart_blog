402f9ac55c25da3519714e72dd09fa32c3998bf0
Laumlin <1255964530@qq.com>
2018-06-14 22:48:38 +0800

    初始化

---
+++ src/components/article.vue
@@ -0,0 +1,45 @@
+<template>
+	<article>
+		<h2><router-link :to="'/atricle/' + article.id" class="article-title">{{ article.title }}</router-link></h2>
+		<p><router-link :to="'/atricle/' + article.author.id" class="article-author">{{ article.author.name }}</router-link></p>
+		<span class="article-date">时间:{{ article.date }}</span>
+		<p class="article-content">{{ article.content }}</p>
+		<router-link :to="'/atricle/' + article.id" >查看全文</router-link>
+	</article>
+</template>
+
+<script>
+export default {
+	props: {
+		article: Object
+	}
+}
+</script>
+
+<style scoped>
+article {
+	width: 100%;
+	padding: 10px;
+	border-bottom: 1px solid #000;
+}
+	.article-title {
+		font-size: 28px;
+		color: #000;
+	}
+	.article-author {
+		color: #000;
+	}
+	.article-title:hover, .article-author:hover {
+		color: #0581e1;
+	}
+	.article-date {
+		font-size: 16px;
+	}
+	.article-content {
+		margin: 20px 100px;
+		text-overflow: ellipsis;
+		color: #000;
+		white-space: nowrap;
+		overflow: hidden;
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 import Home from '@/view/Home'
 
+const Login = () => import ('@/view/view-login')
+const ArticlesList = () => import ('@/view/view-articles-list') 
+const ArticlesDetail = () => import ('@/view/view-articles-detail') 
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/article',
+      name: 'articles-list',
+      component: ArticlesList
+    },
+    {
+      path: '/article/:id',
+      name: 'articles-detail',
+      component: ArticlesDetail
     }
   ]
 })
