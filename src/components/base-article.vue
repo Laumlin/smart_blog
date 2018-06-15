2f29772bdb207d560093c418f33eaaae7e6dbbbd
ruofee <768047505@qq.com>
2018-06-16 00:33:30 +0800

    保存格式并修改跳转方式

--- src/components/base-article.vue
+++ src/components/base-article.vue
@@ -1,10 +1,10 @@
 <template>
 	<article>
-		<h2><router-link :to="'/atricle/' + article.id" class="title">{{ article.title }}</router-link></h2>
+		<h2><router-link :to="{name: 'home'}" class="title">{{ article.title }}</router-link></h2>
 		<!-- <span><router-link :to="'/atricle/' + article.author.id" class="author">{{ article.author.name }}</router-link></span> -->
 		<span class="date">时间:{{ article.date }}</span>
 		<p class="content">{{ article.content }}</p>
-		<router-link :to="'/atricle/' + article.id" >查看全文</router-link>
+		<router-link :to="{ name: 'article', params: {id: article.id} }" >查看全文</router-link>
 	</article>
 </template>
 
@@ -29,7 +29,7 @@ article {
 	.author, .date {
 		margin: 8px;
 		padding: 5px;
-		font-size: 14px;	
+		font-size: 14px;
 		color: #000;
 	}
 	.title:hover, .author:hover {
