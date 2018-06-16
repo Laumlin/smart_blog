d14cc8740bae36623efc1b6a75e302f0f8c7026f
ruofee <768047505@qq.com>
2018-06-16 15:16:20 +0800

    将mock中对象从外部引入, 便于对对象的管理

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,24 +1,5 @@
 const Mock = require('mockjs')
-const Random = Mock.Random
-const produceNewData = function () {
-	let articles = [];
-	for (let i = 0; i < 10; i++) {
-		let newArticle = {
-			title: Random.csentence(5, 30),
-			id: Random.natural(),
-			content: Random.cparagraph(100, 400),
-			date:  Random.date() + ' ' + Random.time(),
-			author: {
-				name: Random.cname(),
-				id: Random.natural()
-			}
-		}
-		articles.push(newArticle)
-	}
 
-	return {
-		articleList: articles
-	}
-}
+import {articles} from './articles'
 
-Mock.mock('https://test.com/article', 'get', produceNewData);
+Mock.mock('https://test.com/articles', 'get', articles)
