6844cf860e77aac716cf51cf95135a826e0c1461
ruofee <768047505@qq.com>
2018-06-16 22:52:19 +0800

    引入mock伪链接article详情

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -2,6 +2,8 @@ const Mock = require('mockjs')
 
 import {articles} from './articles'
 import {user} from './user'
+import {article} from './article'
 
 Mock.mock('https://test.com/login', 'post', user)
 Mock.mock('https://test.com/articles', 'get', articles)
+Mock.mock('https://test.com/article', 'get', article)
