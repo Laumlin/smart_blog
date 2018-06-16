a478ce736a0a3ae5f1354c30c8e5743c17903cbe
ruofee <768047505@qq.com>
2018-06-17 00:08:07 +0800

    设置顶部栏: 创建顶部栏组件(未完善)

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
   <div id="app">
     <notifications group="foo" position="top center"></notifications>
     <!-- <img src="./assets/images/logo.png"> -->
+    <the-nav />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+const TheNav = () => import('@/components/the-nav')
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TheNav
+  }
 }
 </script>
 
---
+++ src/components/the-nav.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="nav">
+    <div class="logo-wrap">
+      <img class="logo" :src="logo.src" alt="Logo">
+    </div>
+    <div class="admin">
+      <button class=""></button>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'the-nav',
+    data () {
+      return {
+        logo: {
+          src: '@/assets/images/logo.png'
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .nav {
+    position: fixed;
+    top: 0;
+    height: 60px;
+    background-color: #030303;
+  }
+</style>
