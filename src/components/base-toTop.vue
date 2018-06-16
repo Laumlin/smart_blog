068f39515f17aa254f404c16c56726da4fb71ce4
Laumlin <1255964530@qq.com>
2018-06-16 23:20:16 +0800

    新增back-toTop.vue

---
+++ src/components/base-toTop.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <button class="back-top" @click="backTop()" v-show="backTopShow">toTop</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      backTopShow: false
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    backTop () {
+      let back = setInterval(() => {
+        if (document.body.scrollTop || document.documentElement.scrollTop){
+          document.body.scrollTop -= 100
+          document.documentElement.scrollTop -= 100
+        } else {
+          clearInterval(back)
+        }
+      })
+    }
+  },
+  handleScroll () {
+    if (document.documentElement.scrollTop + document.body.scrollTop > 100) {
+      this.backTopShow = true
+    } else {
+      this.backTopShow = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.back-top {
+  position: fixed;
+  right: 10px;
+  bottom: 10px;
+}
+</style>
