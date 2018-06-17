ed6ba7423dd9ae7b555b6134647b6edeae9089f5
ruofee <768047505@qq.com>
2018-06-17 21:16:26 +0800

    基本完善nav(无数据支持)

--- src/components/the-nav.vue
+++ src/components/the-nav.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="nav">
     <div class="logo-wrap">
-      <img class="logo" :src="logo.src" alt="Logo">
+      <img class="logo" src="@/assets/images/logo.png" alt="Logo">
     </div>
     <div class="admin">
-      <button class=""></button>
+      <button class="admin-button"><router-link class="admin-button-link" :to="{name: 'login'}">登录</router-link></button>
     </div>
   </nav>
 </template>
@@ -14,9 +14,6 @@
     name: 'the-nav',
     data () {
       return {
-        logo: {
-          src: '@/assets/images/logo.png'
-        }
       }
     }
   }
@@ -26,7 +23,41 @@
   .nav {
     position: fixed;
     top: 0;
+    left: 0;
+    display: flex;
+    width: 100%;
     height: 60px;
-    background-color: #030303;
+    background-color: #fff;
+    box-shadow: 1px 1px 1px #3333337d;
   }
+    .logo-wrap {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 120px;
+      height: 60px;
+    }
+      .logo {
+        width: 50px;
+        height: 50px;
+      }
+    .admin {
+      display: flex;
+      justify-content: flex-end;
+      align-items: center;
+      width: calc(100% - 120px);
+    }
+      .admin-button {
+        width: 60px;
+        height: 60px;
+        color: #333;
+        border: none;
+        background-color: #fff;
+        outline: none;
+      }
+        .admin-button-link {
+        }
+        .admin-button-link:hover {
+          text-decoration: underline;
+        }
 </style>
