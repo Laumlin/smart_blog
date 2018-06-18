0ff21d8bbe93e9fb5e9bfc69ca363315368143e8
ruofee <768047505@qq.com>
2018-06-18 20:32:04 +0800

    新增清空vuex中文章详情和文章列表的mutation

--- src/store/module/article.js
+++ src/store/module/article.js
@@ -3,7 +3,7 @@ import http from '@/api'
 export default {
   state: {
     articles: [],
-    article_current: {}
+    article_current: null
   },
   mutations: {
     setArticles (state, articles) {
@@ -14,6 +14,12 @@ export default {
     },
     setArticle (state, article) {
       state.article_current = article
+    },
+    removeArticles (state) {
+      state.articles = []
+    },
+    removeArticle (state) {
+      state.article_current = null
     }
   },
   actions: {
@@ -21,13 +27,18 @@ export default {
       // return http.get('https://test.com/articles', {
       //   page: query.page
       // })
-      return http.get('https://test.com/articles')
+      return http.get('http://172.18.5.196:8080/Smart_Blog/articles', {
+        page: query.page
+      })
     },
     getArticle ({state, commit}, params) {
       // return http.get('https://test.com/article/', {
       //   id: params.id
       // })
-      return http.get('https://test.com/article')
+      // return http.get('https://test.com/article')
+      return http.get('http://172.18.5.196:8080/Smart_Blog/article/', {
+        id: params.id
+      })
     }
   }
 }
