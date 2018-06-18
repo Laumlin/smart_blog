8a9a7d77feaf02fbac3c847d757e8cc84a3da839
ruofee <768047505@qq.com>
2018-06-18 15:26:56 +0800

    添加setUser, 以便和setUserCurrent做出区别

--- src/store/module/user.js
+++ src/store/module/user.js
@@ -6,8 +6,11 @@ export default {
     user: {}
   },
   mutations: {
-    setUser (state, user) {
+    setUserCurrent (state, user) {
       state.user_current = user
+    },
+    setUser (state, user) {
+      state.user = user
     },
     loginout (state, user) {
       state.user_current = {}
@@ -15,7 +18,8 @@ export default {
   },
   actions: {
     login ({state, commit}, data) {
-      return http.post('http://172.18.5.196:8080/Smart_Blog/login/', data)
+      // return http.post('http://172.18.5.196:8080/Smart_Blog/login/', data)
+      return http.post('https://test.com/login')
     }
   }
 }
