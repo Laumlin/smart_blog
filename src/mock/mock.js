4bbf9d1e2c557708f4a43e0075f9d9b76532500e
ruofee <768047505@qq.com>
2018-06-18 17:10:12 +0800

    添加loginout测试接口

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -8,4 +8,5 @@ import {article} from './article'
 Mock.mock('https://test.com/login', 'post', user)
 Mock.mock('https://test.com/articles', 'get', articles)
 Mock.mock('https://test.com/article', 'get', article)
+Mock.mock('https://test.com/loginout', 'post')
 
