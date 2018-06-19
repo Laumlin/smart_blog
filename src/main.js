06459df277c90c62eb2af70eb797da708777a5ba
ruofee <768047505@qq.com>
2018-06-19 16:37:59 +0800

    引入三方包: markdown编辑器(mavon-editor)

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import MavonEditor from 'mavon-editor'
 import Notification from 'vue-notification'
 import filter from '@/init/filter'
 import './api'
 import "@/mock/mock"
 import hljs from 'highlight.js'
-import 'highlight.js/styles/googlecode.css'
 
 Vue.directive('highlight', (el) => {
     let blocks = el.querySelectorAll('pre code')
@@ -21,6 +21,8 @@ Vue.directive('highlight', (el) => {
 import '@/assets/css/default.css'
 import '@/assets/css/github-markdown.css'
 import '@/assets/iconfont/iconfont.css'
+import 'highlight.js/styles/googlecode.css'
+import 'mavon-editor/dist/css/index.css'
 
 for (let key of Object.keys(filter)) {
   Vue.filter(key, filter[key])
@@ -28,6 +30,7 @@ for (let key of Object.keys(filter)) {
 
 Vue.config.productionTip = false
 Vue.use(Notification)
+Vue.use(MavonEditor)
 
 /* eslint-disable no-new */
 new Vue({
