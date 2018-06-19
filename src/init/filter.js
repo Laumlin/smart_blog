072dcc6c20da31b2963cd420763495d8b074208e
ruofee <768047505@qq.com>
2018-06-19 23:29:13 +0800

    修复filter中textCut中遇到比截取字数小的string仍然加...的bug

--- src/init/filter.js
+++ src/init/filter.js
@@ -3,7 +3,13 @@ const filter = {
     return new Date(date).toLocaleString()
   },
   textCut (string, number, defaultText) {
-    return string.substr(0, number) + '...' || defaultText
+    if (string.length > number) {
+      return string.substr(0, number) + '...'
+    } else if (string.length <= number) {
+      return string
+    } else {
+      return defaultText
+    }
   }
 }
 
