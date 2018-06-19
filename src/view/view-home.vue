4cc53c90ef9e7adaf279ea566d46e08cb36246f6
ruofee <768047505@qq.com>
2018-06-19 20:54:18 +0800

    为home页面添加拉取下一页的功能

--- src/view/view-home.vue
+++ src/view/view-home.vue
@@ -4,6 +4,10 @@
       <div class="article-wrap" v-for="(article, key) in articles" :key="key">
         <base-article :article="article" />
       </div>
+      <footer class="footer">
+        <span v-if="end">No more data :)</span>
+        <span @click="load" v-else><i class="iconfont more-icon">&#xe630;</i></span>
+      </footer>
     </div>
 	</div>
 </template>
@@ -15,37 +19,55 @@ export default {
 	name: 'view-home',
 	data () {
 		return {
-      page: 1
+      page: 1,
+      end: false
 		}
 	},
+  components: {
+    BaseArticle
+  },
   computed: {
     articles () {
-      return this.$store.state.article.articles.articles
+      return this.$store.state.article.articles
+    }
+  },
+  methods: {
+    getArticles (page) {
+      this.$store.dispatch('getArticles', {
+        page
+      })
+        .then((res) => {
+          console.log(res)
+          if (this.page == 1) {
+            this.$store.commit('setArticles', res.articles)
+            console.log('set')
+          } else {
+            console.log('add')
+            this.$store.commit('addArticles', res.articles)
+          }
+          console.log('next', res.next)
+          if (res.next) {
+            this.page++
+            console.log(this.page)
+          } else {
+            this.end = true
+          }
+        })
+        .catch((err) => {
+          this.$notify({
+            type: 'error',
+            title: '请重试',
+            text: '获取文章失败'
+          })
+        })
+    },
+    load () {
+      this.getArticles(this.page)
     }
   },
 	mounted () {
-    this.$store.dispatch('getArticles', {
-      page: this.page
-    })
-      .then((res) => {
-        if (this.page == 1) {
-          this.$store.commit('setArticles', res)
-          this.page++
-        } else {
-          this.$store.commit('addArticles', res)
-        }
-      })
-      .catch((err) => {
-        this.$notify({
-          type: 'error',
-          title: '请重试',
-          text: '获取文章失败'
-        })
-      })
-	},
-	components: {
-		BaseArticle
-	},
+    this.getArticles(1)
+	}
 }
 </script>
 
@@ -55,12 +77,25 @@ export default {
   align-items: center;
   flex-direction: column;
   margin-top: 80px;
-  margin-bottom: 40px;
+  margin-bottom: 10px;
 }
   .home {
     width: 850px;
   }
     .article-wrap {
       margin-bottom: 10px;
+    }
+  .footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 60px;
+    font-size: .9em;
+    color: #999;
+  }
+    .more-icon {
+      font-size: 3em;
+      color: #59b982;
+      cursor: pointer;
     }
 </style>
