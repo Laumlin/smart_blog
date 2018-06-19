a2ab38f5a17d2dbc081010c862158f3fdd8262dd
ruofee <768047505@qq.com>
2018-06-20 00:38:49 +0800

    删除不必要测试代码

--- src/store/module/article.js
+++ src/store/module/article.js
@@ -30,9 +30,6 @@ export default {
       // })
     },
     getArticle ({state, commit}, params) {
-      // return http.get('https://test.com/article/', {
-      //   id: params.id
-      // })
       return http.get('https://test.com/article')
       // return http.get('http://172.18.5.196:8080/Smart_Blog/article/', {
       //   id: params.id
